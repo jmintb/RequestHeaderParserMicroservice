0c127b7bbf3b0be40ac56f0a580097160c36a532
Jessie Chatham Spencer <isai@protonmail.com>
2017-06-19 14:30:23 +0200

    Moved the request header parsing functionality to its own module.

---
+++ app/api/RequestHeaderParser.js
@@ -0,0 +1,17 @@
+module.exports = function getSystemInfo(app) {
+    app.get('/', function (req, res) {
+    res.json(getSystemInfo(req));
+});
+
+function getSystemInfo(req) {
+  return {
+    ip: req.headers['x-forwarded-for'] ||
+     req.connection.remoteAddress ||
+     req.socket.remoteAddress ||
+     req.connection.socket.remoteAddress,
+    language: req.headers["accept-language"].split(',')[0],
+    os:  req.headers['user-agent'].split('(')[1].split(')')[0]
+  };
+}
+
+}
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 var express = require('express');
 var app = express();
+var requestHeaderParser = require('app/api/RequestHeaderParser.js');
 var port = process.env.PORT || 8080;
 
 app.use(function (req, res, next) {
@@ -9,21 +10,10 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Methods", "GET");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
-})
-
-app.get('/', function (req, res) {
-  res.json(getSystemInfo(req));
 });
+
+requestHeaderParser(app);
 
 app.listen(port);
 
-function getSystemInfo(req) {
-  return {
-    ip: req.headers['x-forwarded-for'] ||
-     req.connection.remoteAddress ||
-     req.socket.remoteAddress ||
-     req.connection.socket.remoteAddress,
-    language: req.headers["accept-language"].split(',')[0],
-    os:  req.headers['user-agent'].split('(')[1].split(')')[0]
-  };
-}
+
